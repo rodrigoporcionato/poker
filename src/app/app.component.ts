fac4c8f5ed4de5eb54d91b1e72c1610cec881504
RodrigoPorcionato <rodrigoporcionato@gmail.com>
2020-02-22 16:17:54 -0300

    add new poker

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pokerplan';
+
+  constructor() { }
+  
+  list = ['0', '1','2','3','5','8','13','21','34','55','89','144','endless','?','cup'];
+  cardSelected = '';
+  cardIsOpen: boolean;
+
+  ngOnInit(): void {    
+  }
+
+  open(cardSelected) {
+    this.cardIsOpen = true;
+    this.cardSelected = cardSelected;
+  }
+  close(){
+    this.cardIsOpen = false;
+  }
+
+}
